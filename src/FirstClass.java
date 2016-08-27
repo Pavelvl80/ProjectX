6004dd0f902590a20b83d95a4d1fe322861d1711
Pavelvl80 <komp897@mail.ru>
2016-08-27 16:28:30 +0200

    First commit

---
+++ src/FirstClass.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Edvard Piri on 27.08.2016.
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+
+        int a = 10;
+        String test = "test";
+        System.out.println(a );
+    }
+}
